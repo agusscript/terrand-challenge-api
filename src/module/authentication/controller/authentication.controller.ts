4f0d5d0cb7acfe0dbea9deb361a3f602b67d5f13
agusscript <agus.sanchez.240@gmail.com>
2025-05-02 23:25:31 -0300

    Add explicit return types to sign-in and sign-up methods in authentication module

--- src/module/authentication/controller/authentication.controller.ts
+++ src/module/authentication/controller/authentication.controller.ts
@@ -2,19 +2,25 @@ import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards }
 import { AuthenticationService } from "../service/authentication.service";
 import { SignUpDto } from "../dto/signup.dto";
 import { SignInDto } from "../dto/signIn.dto";
+import { ISignInResponse } from "../interface/sign-in-response.interface";
+import { User } from "src/module/user/entity/user.entity";
 
 @Controller("auth")
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) { }
 
   @Post("sign-up")
-  async signUp(@Body() signUpDto: SignUpDto) {
+  async signUp(
+    @Body() signUpDto: SignUpDto
+  ): Promise<User> {
     return this.authenticationService.signUp(signUpDto);
   }
 
   @Post("sign-in")
   @HttpCode(HttpStatus.OK)
-  async signIn(@Body() signInDto: SignInDto) {
+  async signIn(
+    @Body() signInDto: SignInDto
+  ): Promise<ISignInResponse> {
     return this.authenticationService.signIn(signInDto);
   }
 }
