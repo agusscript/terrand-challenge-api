4cf1e046c7197c031ab0585f60803a8d2ce2a3e4
agusscript <agus.sanchez.240@gmail.com>
2025-05-07 00:54:44 -0300

    Add length validations to title and description properties in create-recipe DTO

--- src/module/recipe/dto/create-recipe.dto.ts
+++ src/module/recipe/dto/create-recipe.dto.ts
@@ -1,12 +1,16 @@
-import { IsArray, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { IsArray, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
 
 export class CreateRecipeDto {
   @IsNotEmpty()
   @IsString()
+  @MinLength(2)
+  @MaxLength(50)
   title: string;
 
   @IsNotEmpty()
   @IsString()
+  @MinLength(2)
+  @MaxLength(250)
   description: string;
 
   @IsArray()
